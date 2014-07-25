3392505709ef4ad41f30996dff546159d7da16b2
Ian Foote <ian@incuna.com>
2014-07-25 11:14:32 +0200

    Fixture scoping
    py.test code/test_fixture_cache_scope.py
    * Slow fixture called once at module scope
    * Called twice at function scope
    
    py.test --durations=10
    * Show 10 slowest calls - splits between setup, call, teardown for each
      test function

--- pytest-tutorial/code/test_fixture_cache_scope.py
+++ pytest-tutorial/code/test_fixture_cache_scope.py
@@ -1,13 +1,16 @@
 import time
 import pytest
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope="function")  # module, class, session
 def mod():
     time.sleep(1)
     return 1
 
+
 def test_func(mod):
     assert mod == 1
+
 
 def test_func2(mod):
     assert mod == 1
