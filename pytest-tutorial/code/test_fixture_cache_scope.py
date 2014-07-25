89534b5a4e007406bc15560b1d729ae166105abc
Ian Foote <ian@incuna.com>
2014-07-25 11:24:50 +0200

    yield based alternative finalizer

--- pytest-tutorial/code/test_fixture_cache_scope.py
+++ pytest-tutorial/code/test_fixture_cache_scope.py
@@ -2,13 +2,11 @@ import time
 import pytest
 
 
-@pytest.fixture(scope="function")  # module, class, session
-def mod(request):
-    def pr():
-        print('Finished')
-    request.addfinalizer(pr)
+@pytest.yield_fixture(scope="function")  # module, class, session
+def mod():
     time.sleep(1)
-    return 1
+    yield 1
+    print('Finished')
 
 
 def test_func(mod):
