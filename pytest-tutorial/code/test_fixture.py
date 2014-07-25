e34762cc535d4415bbf2a69fed027c23f968e050
Ian Foote <ian@incuna.com>
2014-07-25 10:54:50 +0200

    Add docstring to somevalue fixture; Add skip
    py.test --fixture code/test_fixture.py

--- pytest-tutorial/code/test_fixture.py
+++ pytest-tutorial/code/test_fixture.py
@@ -1,8 +1,12 @@
 import pytest
 
+
 @pytest.fixture
 def somevalue():
+    """Always returns the value 42"""
+    pytest.skip("skipped")
     return 42
+
 
 def test_function(somevalue):
     assert somevalue == 42
