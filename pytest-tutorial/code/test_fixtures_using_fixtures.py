959d002f830b4e90615ce893952eff13b67b4ea9
Ian Foote <ian@incuna.com>
2014-07-25 11:50:42 +0200

    Fixture composition
    py.test code/test_fixtures_using_fixtures.py

--- pytest-tutorial/code/test_fixtures_using_fixtures.py
+++ pytest-tutorial/code/test_fixtures_using_fixtures.py
@@ -1,9 +1,15 @@
 import pytest
 
-@pytest.fixture
+
+@pytest.fixture(params=(10, 20))
 def answer(request):
-    return 21
+    return 21 * request.param
+
 
 @pytest.fixture
 def answer2(answer):
     return answer * 2
+
+
+def test_answer(answer2):
+    assert answer2 == 420
